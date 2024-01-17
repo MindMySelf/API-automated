2fd52600fbcdb45a42b5f54c5215293438cf4ba8
Csege <cseloteigergely@gmail.com>
2024-01-17 09:21:34 +0100

    add SharedVariables class with base url and endpoints

---
+++ src/test/java/SharedVariables.java
@@ -0,0 +1,6 @@
+public class SharedVariables {
+    private String basicURL = "https://restful-booker.herokuapp.com";
+    private String authEndpoint = "/auth";
+    private String bookingEndpoint = "/booking";
+    private String pingEndpoint = "/ping";
+}
