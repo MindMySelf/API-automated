e9c5b5386542241cf5c52cd19bf19db68cf3d3e4
Csege <cseloteigergely@gmail.com>
2024-01-17 11:22:58 +0100

    add tests to check endpoints existence to SmokeTest class

--- src/test/java/smokeTests/SmokeTest.java
+++ src/test/java/smokeTests/SmokeTest.java
@@ -1,4 +1,45 @@
 package smokeTests;
 
+import com.MindMySelf.SharedVariables;
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.when;
+
 public class SmokeTest {
+    private final SharedVariables sharedVariables = new SharedVariables();
+
+    private void setBaseURL() {
+        RestAssured.baseURI = sharedVariables.basicURL;
+    }
+
+    @Test
+    public void authEndpointExistTest() {
+        setBaseURL();
+        when().post(sharedVariables.authEndpoint).
+                then().
+                assertThat().
+                statusCode(415);
+    }
+
+    @Test
+    public void bookGetEndPointExistsTest() {
+        setBaseURL();
+        when().
+                get(sharedVariables.bookingEndpoint).
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void pingGetEndpointExistsTest() {
+        setBaseURL();
+        when().
+                get(sharedVariables.pingEndpoint).
+                then().
+                assertThat().
+                statusCode(201);
+    }
+
 }
