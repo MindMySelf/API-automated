7e87fa2a237069da705962493f31e932cf4267a6
Csege <cseloteigergely@gmail.com>
2024-01-17 11:01:48 +0100

    refactor access modifiers to public in SharedVariables

---
+++ src/main/java/com/MindMySelf/SharedVariables.java
@@ -0,0 +1,8 @@
+package com.MindMySelf;
+
+public class SharedVariables {
+    public String basicURL = "https://restful-booker.herokuapp.com";
+    public String authEndpoint = "/auth";
+    public String bookingEndpoint = "/booking";
+    public String pingEndpoint = "/ping";
+}
