b2394aa66b90befd57bdc7f426d492eac3fa970c
Csege <cseloteigergely@gmail.com>
2024-01-17 15:16:30 +0100

    add usernameAndPasswordAuthTest to SmokeTest class

--- src/test/java/SharedVariables.java
+++
@@ -1,6 +0,0 @@
-public class SharedVariables {
-    private String basicURL = "https://restful-booker.herokuapp.com";
-    private String authEndpoint = "/auth";
-    private String bookingEndpoint = "/booking";
-    private String pingEndpoint = "/ping";
-}
--- src/test/java/smokeTests/SmokeTest.java
+++ src/test/java/smokeTests/SmokeTest.java
@@ -4,6 +4,7 @@ import com.MindMySelf.SharedVariables;
 import io.restassured.RestAssured;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class SmokeTest {
@@ -16,30 +17,45 @@ public class SmokeTest {
     @Test
     public void authEndpointExistTest() {
         setBaseURL();
-        when().post(sharedVariables.authEndpoint).
-                then().
-                assertThat().
-                statusCode(415);
+        when().post(sharedVariables.authEndpoint)
+                .then()
+                .assertThat()
+                .statusCode(415);
     }
 
     @Test
     public void bookGetEndPointExistsTest() {
         setBaseURL();
         when().
-                get(sharedVariables.bookingEndpoint).
-                then().
-                assertThat().
-                statusCode(200);
+                get(sharedVariables.bookingEndpoint)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .log().all();
     }
 
     @Test
     public void pingGetEndpointExistsTest() {
         setBaseURL();
-        when().
-                get(sharedVariables.pingEndpoint).
-                then().
-                assertThat().
-                statusCode(201);
+        when()
+                .get(sharedVariables.pingEndpoint)
+                .then()
+                .assertThat()
+                .statusCode(201);
+    }
+
+    @Test
+    public void usernameAndPasswordAuthTest() {
+        setBaseURL();
+        given()
+                .header("Content-Type", "application/json")
+                .body("{\"username\": \"admin\", \"password\": \"password123\"}")
+                .when()
+                .post(sharedVariables.authEndpoint)
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .log().all();
     }
 
 }
