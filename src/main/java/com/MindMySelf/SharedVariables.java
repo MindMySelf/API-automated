d06846104734221228786c17a7c4158bab9e0b9e
Csege <cseloteigergely@gmail.com>
2024-01-18 15:20:01 +0100

    add http status codes to SharedVariables.java

--- src/main/java/com/MindMySelf/SharedVariables.java
+++ src/main/java/com/MindMySelf/SharedVariables.java
@@ -5,4 +5,8 @@ public final class SharedVariables {
     public static String authEndpoint = "/auth";
     public static String bookingEndpoint = "/booking";
     public static String pingEndpoint = "/ping";
+    public static int badRequest = 400;
+    public static int UnsupportedMediaType = 415;
+    public static int OK = 200;
+    public static int created = 201;
 }
