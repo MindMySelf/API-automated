f5570cb3f6c061765bc9678a24ab06276c5e89f3
Csege <cseloteigergely@gmail.com>
2024-01-18 16:46:29 +0100

    add bookingGetByIdWithNegativeIDTest test to BookingGetFeatureTest.java

--- src/test/java/featureTests/BookingGetFeatureTest.java
+++ src/test/java/featureTests/BookingGetFeatureTest.java
@@ -2,9 +2,23 @@ package featureTests;
 
 import com.MindMySelf.SharedVariables;
 import io.restassured.RestAssured;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.when;
 
 public class BookingGetFeatureTest {
     private void setBaseURL() {
         RestAssured.baseURI = SharedVariables.basicURL;
+    }
+
+    @Test
+    public void bookingGetByIdWithNegativeIDTest() {
+        setBaseURL();
+        when()
+                .get(SharedVariables.bookingEndpoint + "/-1")
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .log().all();
     }
 }
