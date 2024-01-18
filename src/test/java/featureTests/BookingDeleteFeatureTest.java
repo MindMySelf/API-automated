79ad4edeb5bec702a53b9a623bab509990126395
Csege <cseloteigergely@gmail.com>
2024-01-18 22:43:59 +0100

    add BookingDeleteFeatureTest.java, add passing noHeaderDeleteRequestToBookingTest test case to it

---
+++ src/test/java/featureTests/BookingDeleteFeatureTest.java
@@ -0,0 +1,37 @@
+package featureTests;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.MindMySelf.SharedVariables.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class BookingDeleteFeatureTest {
+    private static String jsonBody = """
+                {
+                    "firstname" : "Test",
+                    "lastname" : "Testtest",
+                    "totalprice" : 123,
+                    "depositpaid" : true,
+                    "bookingdates" : {
+                        "checkin" : "2024-01-01",
+                        "checkout" : "2024-01-11"
+                    },
+                    "additionalneeds" : "Lunch"
+                }""";
+    @Test
+    public void noHeaderDeleteRequestToBookingTest() {
+        setBaseURL();
+        List<Response> tokenAndID = setupAuthAndCreateBooking(jsonBody);
+        int id = tokenAndID.get(1).path("bookingid");
+        when()
+                .delete(bookingEndpoint + "/" + id)
+                .then()
+                .assertThat()
+                .statusCode(forbidden)
+                .log().all();
+    }
+}
