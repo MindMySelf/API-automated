9bf529010fa2d2d0a489fa3712178e85b91c4b2d
Csege <cseloteigergely@gmail.com>
2024-01-18 21:44:01 +0100

    add BookingPutFeatureTest.java, add passing noHeaderNoBodyPutRequestToBookingTest test case to it

---
+++ src/test/java/featureTests/BookingPutFeatureTest.java
@@ -0,0 +1,48 @@
+package featureTests;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.MindMySelf.SharedVariables.*;
+import static io.restassured.RestAssured.when;
+
+public class BookingPutFeatureTest {
+    private static String jsonBody = """
+                {
+                    "firstname" : "Ödön",
+                    "lastname" : "Szerencsi",
+                    "totalprice" : 987,
+                    "depositpaid" : true,
+                    "bookingdates" : {
+                        "checkin" : "2024-01-01",
+                        "checkout" : "2024-01-16"
+                    },
+                    "additionalneeds" : "Lunch"
+                }""";
+    private static String putJsonBody = """
+                {
+                    "firstname" : "Lilla",
+                    "lastname" : "Lali",
+                    "totalprice" : 9999,
+                    "depositpaid" : false,
+                    "bookingdates" : {
+                        "checkin" : "2010-11-01",
+                        "checkout" : "2011-11-21"
+                    },
+                    "additionalneeds" : "Dinner"
+                }""";
+    @Test
+    public void noHeaderNoBodyPutRequestToBookingTest() {
+        setBaseURL();
+        List<Response> tokenAndID = setupAuthAndCreateBooking(jsonBody);
+        int id = tokenAndID.get(1).path("bookingid");
+        when()
+                .put(bookingEndpoint + "/" + id)
+                .then()
+                .assertThat()
+                .statusCode(forbidden)
+                .log().body();
+    }
+}
